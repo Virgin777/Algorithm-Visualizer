d72e7ba1aff1545ca4503f25f77392ad051f2f4d
sahaya virgin mary <146453232+Virgin777@users.noreply.github.com>
2025-10-28 11:47:45 +0530

    Delete start-dev.bat

--- start-dev.bat
+++
@@ -1,9 +0,0 @@
-@echo off
-echo Starting Algorithm Visualizer Development Server...
-echo.
-echo The application will be available at:
-echo   http://localhost:5173/
-echo.
-echo Press Ctrl+C to stop the server
-echo.
-npm run dev
